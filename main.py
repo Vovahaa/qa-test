16362e0c0652a8e75b1cc7c139c50a43fad16073
unknown <markovka1198@gmail.com>
2022-08-24 17:14:44 +0300

    3

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-x = "Hello, "
-y = "my dear "
-z = "Friend."
-print(x+y+z)
+def myfunc(x, y):
+    q = x + y
+    w = x * y
+    r = x - y
+    t = x / y
+    print(x, y, q, w, r, t)
+
+
+x = int(input())
+y = int(input())
+myfunc(x, y)
